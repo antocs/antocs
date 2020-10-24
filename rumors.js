9e3632c73196aca5496f321e64bfaedf317ac2de
antocs <roxasthethund@yahoo.it>
2020-10-24 22:52:42 +0200

    Create rumors.js

---
+++ rumors.js
@@ -0,0 +1,66 @@
+export const rumors = [
+  {
+    name: ["Story Event: The Eccentric Electrician",
+          "Volkner & Luxray Spotlight Scout [with grid and EX]"],
+    date: "September 30, 2020",
+    expires: "October 15, 2020"
+  },
+  {
+    name: ["Elesa & Zebstrika [with grid and EX] and Clemont & Eliolisk Spotlight Scout"],
+    date: "October 2, 2020",
+    expires: "October 15, 2020"
+  },
+  {
+    name: ["Wallace & Milotic and Sygna Suit Grimsley & Sharpedo [with EX] Spotlight Scout",
+          "Legendary Arena - Entei"],
+    date: "October 5, 2020",
+    expires: "October 17, 2020"
+  },
+  {
+    name: ["Training Area Reward Boost Rally"],
+    date: "October 9, 2020",
+    expires: "October 15, 2020"
+  },
+  {
+    name: ["Story Event: Fight or Fright",
+          "Acerola & Mimikyu Seasonal Scout [with grid and EX]",
+          "Hilbert & Mightyena Seasonal Scout [with grid and EX]"],
+    date: "October 15, 2020",
+    expires: "November 2, 2020"
+  },
+  {
+    name: ["Legendary Arena - Cobalion"],
+    date: "October 17, 2020",
+    expires: "October 29, 2020"
+  },
+  {
+    name: ["Morty & Drifblim [with EX] and Fantina & Mismagius [with grid] Spotlight Scout"],
+    date: "October 19, 2020",
+    expires: "November 2, 2020"
+  },
+  {
+    name: ["Wally & Gallade and Glacia & Glalie [with grid] Spotlight Scout"],
+    date: "October 22, 2020",
+    expires: "November 2, 2020"
+  },
+  {
+    name: ["Ice- and Fighting-type Egg Event"],
+    date: "October 22, 2020",
+    expires: "November 5, 2020"
+  },
+  {
+    name: ["Legendary Arena - Latios"],
+    date: "October 29, 2020",
+    expires: "November 11, 2020"
+  },
+  {
+    name: ["Sygna Suit Blue & Blastoise [with grid & EX]",],
+    date: "August 28, 2020",
+    expires: "October 30, 2020"
+  },
+  {
+    name: ["Special Event: Blasting Off Again! [Free James & Koffing]"],
+    date: "September 17, 2020",
+    expires: "January 25, 2021"
+  }
+];
